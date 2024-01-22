95f8d45fd7f9b45566883f343f303c9329780188
AutiPrathamesh5288 <prathameshauti004@gmail.com>
2024-01-22 14:06:35 +0530

    All Assignments Inserted

---
+++ 01_Basic_Assignments/myapp/lib/assignment1.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Assignment1 extends StatelessWidget {
+  const Assignment1({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("FirstApp")
+        ),
+        body: const Center(
+          child: Text("Hello World!")),
+        )
+    );
+  }
+
+
+}
