2358b97ebfdd024b22d66de44b9b85a6b278a599
AutiPrathamesh5288 <prathameshauti004@gmail.com>
2024-02-25 13:29:15 +0530

    deleted

--- 03_Palindrome_Count_App/palindrome_count_app/README.md
+++
@@ -1,3 +0,0 @@
-# palindrome_count_app
-
-A new Flutter project.
--- 03_Palindrome_Count_App/palindrome_count_app/android/app/src/main/kotlin/com/example/palindrome_count_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.palindrome_count_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
